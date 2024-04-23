a2c00c16678b7b9cd94bf55e1c54fee9ed4672de
jinfanghuang <2283785225@qq.com>
2024-04-23 17:10:14 +0800

    feat: 加载图像

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
 import './style.css';
+import Logo from './logo.png';
 
 function Component () {
   const el = document.createElement("div");
   el.innerHTML = _.join(['webpack', 'demo']);
   el.className = 'hello';
-  console.log('Component')
+  const logo = new Image();
+  logo.src = Logo;
+  el.appendChild(logo);
   return el;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.(css|scss|less)$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource"
       }
     ]
   }
