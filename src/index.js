c5342952394932a536379be9b5a43c5b3f4aa2ef
jinfanghuang <2283785225@qq.com>
2024-04-23 17:00:44 +0800

    feat: 加载 CSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
+import './style.css';
 
 function Component () {
   const el = document.createElement("div");
   el.innerHTML = _.join(['webpack', 'demo']);
+  el.className = 'hello';
   console.log('Component')
   return el;
 }
