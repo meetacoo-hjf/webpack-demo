20ae318ef5c79ef5208d762512e3aaa3922637fc
jinfanghuang <2283785225@qq.com>
2024-04-24 14:48:28 +0800

    feat: 与 async 函数一起使用

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-function getComponent () {
+/*function getComponent() {
   return import('lodash').then(({default: _}) => {
     const el = document.createElement('div');
     el.innerHTML = _.join(['webpack', 'demo'], ' ');
     return el;
   }).catch(err => 'An error occurred while loading the component');
+}*/
+
+// 由于 import() 会返回 promise，与 async 函数一起使用
+async function getComponent() {
+  const {default: _} = await import('lodash');
+  const el = document.createElement('div');
+  el.innerHTML = _.join(['webpack', 'demo'], ' ');
+  return el;
 }
 
 getComponent().then((component) => {
