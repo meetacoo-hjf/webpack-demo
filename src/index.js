a753ac02e372bcc1b5cc86edd5811bdc7bde589b
jinfanghuang <2283785225@qq.com>
2024-04-24 14:44:46 +0800

    feat: 动态导入

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import _ from 'lodash';
-
-function Component () {
-  const el = document.createElement("div");
-  el.innerHTML = _.join(['webpack', 'demo'], ' ');
-  return el;
+function getComponent () {
+  return import('lodash').then(({default: _}) => {
+    const el = document.createElement('div');
+    el.innerHTML = _.join(['webpack', 'demo'], ' ');
+    return el;
+  }).catch(err => 'An error occurred while loading the component');
 }
 
-document.body.appendChild(Component());
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,9 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    another: './src/another-module.js',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    // SplitChunksPlugin 插件可以将公共的依赖模块提取到已有的入口 chunk 中，或者提取到一个新生成的 chunk
-    splitChunks: {
-      chunks: "all"
-    },
   },
 };
