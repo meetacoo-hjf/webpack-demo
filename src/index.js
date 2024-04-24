9128b66fd993c3c52c5013da40ace2f010079af1
jinfanghuang <2283785225@qq.com>
2024-04-24 11:32:45 +0800

    feat: 多入口多输出

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function Component () {
   const el = document.createElement("div");
+  const btn = document.createElement('button');
+
   el.innerHTML = _.join(['webpack', 'demo']);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  el.appendChild(btn);
+
   return el;
 }
 
