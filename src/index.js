879514cedc329a0f51ace93a9c0978e6ffe70c58
jinfanghuang <2283785225@qq.com>
2024-04-24 10:59:05 +0800

    feat: 自定义 JSON 模块解析器

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import './style.css';
 import Logo from './logo.png';
 import Data from './data.xml';
 import Notes from './data.csv';
+import toml from "./data.toml";
+import yaml from "./data.yaml";
+import json from "./data.json5";
+
+console.log(toml.title); // TOML Example
+console.log(toml.owner.name); // Tom Preston-Werner
+console.log(yaml.title); // TOML Example
+console.log(yaml.owner.name); // Tom Preston-Werner
+console.log(json.title); // TOML Example
+console.log(json.owner.name); // Tom Preston-Werner
 
 function Component () {
   const el = document.createElement("div");
