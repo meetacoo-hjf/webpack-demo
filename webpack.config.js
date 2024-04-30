822aebbeadf5d93880cbc92682e41c37d61284ec
jinfanghuang <2283785225@qq.com>
2024-04-30 16:19:26 +0800

    feat: 输出文件的文件名 [contenthash] 将根据资源内容创建唯一哈希值

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Caching"
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js', // [contenthash] 将根据资源内容创建唯一哈希值
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
 };
