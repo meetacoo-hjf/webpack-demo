694942a18398a9a5587ffed9d8ac36f867fe5e2b
jinfanghuang <2283785225@qq.com>
2024-04-30 16:35:50 +0800

    feat: 提取引导模板 不常更改的第三方库提取到单独的 vendor chunk 中

--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true
   },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: { // 将第三方库提取到单独的 vendor chunk 中。这一步将减少客户端对服务器的请求，同时保证自身代码与服务器一致。
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: "all"
+        }
+      }
+    }
+  }
 };
